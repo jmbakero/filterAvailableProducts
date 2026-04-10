9f455ef265964c423b9e97dd5c2b90cc59de6181
jmbakero <jmbakero@gmail.com>
2026-04-10 09:10:37 +0200

    Initial commit with HTML, JS,  CSS, JSON files

---
+++ filterAvailableProducts.js
@@ -0,0 +1,5 @@
+const products = [
+    { name: 'Laptop', price: 320000, category: 'tech', available: true },
+    { name: 'Egér', price: 8000, category: 'tech', available: false },
+    { name: 'Szék', price: 45000, category: 'home', available: true }
+];
