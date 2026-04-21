6b56c1b5abff14ce45e5e8166fc71e9822c7871e
jmbakero <jmbakero@gmail.com>
2026-04-21 09:36:39 +0200

    all filter in one line

--- src/filterAvailableProducts.js
+++ src/filterAvailableProducts.js
@@ -2,9 +2,7 @@ const products = require('./data');
 
 function filterAvailableProducts(products, minPrice, category) {
     return [...products]
-        .filter(product => product.available === true)
-        .filter(product => product.price >= minPrice)
-        .filter(product => product.category === category)
+        .filter(product => product.available === true && product.price >= minPrice && product.category === category)
         .sort((a, b) => a.name.localeCompare(b.name));
 }
 
